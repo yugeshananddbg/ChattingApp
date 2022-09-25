c75000b39f34e0c3aba3f1d2005de7eecf02a509
Yugesh Anand <89369654+yugeshananddbg@users.noreply.github.com>
2022-09-25 10:33:47 +0530

    update

--- chatapp/src/pages/Register.jsx
+++ chatapp/src/pages/Register.jsx
@@ -1,27 +1,28 @@
-import React,{useState} from "react";
-
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Register = () => {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [password1, setPassword1] = useState("")
-    const [profile, setProfile] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [profile, setProfile] = useState("");
   return (
     <>
       <div className="formContainer">
         <div className="formWrapper">
-            <span className="logo">Eureka Chat</span>
-            <span className="logo">Register</span>
+          <span className="logo">Eureka Chat</span>
+          <span className="logo">Register</span>
           <form>
             <input type="text" placeholder="Please Enter your name" />
             <input type="text" placeholder="Please Enter your Email" />
             <input type="text" placeholder="Please Choose Password" />
             <input type="text" placeholder="Confirm Password" />
-            <input type="file" />
-            <button>
-                Register
-            </button>
+            <input style={{ display: "none" }} type="file" id="file" />
+            <label htmlFor="file">
+              <FontAwesomeIcon icon={faImage} />
+            </label>
+            <button>Register</button>
             <p>You do have account ? Login</p>
           </form>
         </div>
