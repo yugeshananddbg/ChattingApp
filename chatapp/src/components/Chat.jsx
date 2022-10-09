b22a8b1b76af3050063beeef7697e98069641134
Yugesh Anand <89369654+yugeshananddbg@users.noreply.github.com>
2022-10-09 13:36:42 +0530

    update

--- chatapp/src/components/Chat.jsx
+++ chatapp/src/components/Chat.jsx
@@ -5,20 +5,25 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/ChatContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Chat = () => {
   const { data } = useContext(ChatContext);
+  const toastHandler = () => {
+    toast("Feature commming soon!");
+  };
 
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>
-          <img src={data.user.photoURL} alt=""  />
+          <img src={data.user.photoURL} alt="" />
           {data.user.displayName}
         </span>
         <div className="chatIcons">
-          <VideocamRoundedIcon className="icon" />
-          <PersonAddIcon className="icon" />
-          <MoreVertIcon className="icon" />
+          <VideocamRoundedIcon className="icon" onClick={toastHandler} />
+          <PersonAddIcon className="icon" onClick={toastHandler} />
+          <MoreVertIcon className="icon" onClick={toastHandler} />
         </div>
       </div>
       <Messages />
